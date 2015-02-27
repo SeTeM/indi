d47fde72b4ece214fe722b36acb716461e5fb660
Max Vasiliev <max@vasiliev.me>
2015-02-27 16:46:30 +0300

    Add gulp pipeline

--- config/application.rb
+++ config/application.rb
@@ -24,5 +24,7 @@ module Indi
     config.active_record.raise_in_transactional_callbacks = true
 
     config.autoload_paths += %W(#{config.root}/lib)
+    config.assets.paths << Rails.root.join("public", "assets", "stylesheets")
+    config.assets.paths << Rails.root.join("public", "assets", "javascripts")
   end
 end
